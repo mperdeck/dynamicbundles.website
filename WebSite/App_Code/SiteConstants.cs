dcffad2190f75e7eb68b7a2ab2c6788d575d1011
mperdeck <mperdeck@gmail.com>
2014-11-15 21:21:08 +1100

    0.5

---
+++ WebSite/App_Code/SiteConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.App_Code
+{
+    public static class SiteConstants
+    {
+        public static string CurrentVersion = Generated.Version;
+        public const string LicenceUrl = "https://github.com/mperdeck/jsnlog/blob/master/LICENSE";
+        public const string LicenceName = "Apache License, Version 2.0";
+
+        public const string NugetDownloadUrl = "http://www.nuget.org/packages/dynamicbundles";
+    }
+}
+
