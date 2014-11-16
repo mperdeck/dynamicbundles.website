2fafdab5b8b1ecf14f5215918b6483c1bc706aca
mperdeck <mperdeck@gmail.com>
2014-11-16 12:01:33 +1100

    0.6

---
+++ WebSite - Copy/App_Code/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ViewExtensions;
+
+namespace WebSite.App_Code
+{
+    public static class Utils
+    {
+    }
+}
+
